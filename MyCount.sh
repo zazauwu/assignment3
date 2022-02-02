a852fbbd93d9f131d6481b0005ff025b9edf7385
zazauwu <zazacrua@gmail.com>
2022-02-02 14:36:24 -0500

    Update MyCount.sh
    add comments for readers

--- MyCount.sh
+++ MyCount.sh
@@ -1,3 +1,7 @@
+#Zhijun Li 20136835
+#The following command is to loop through a sequence of 20 numbers.
+#Within 1 to 20 print out the number and whether it is single- or double-digt
+
 for i in {1..9}
 do
   echo "$i is a single-digit number"
