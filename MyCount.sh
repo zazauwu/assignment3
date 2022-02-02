7a8d91b61cbd0eb51af82059e5a13fb5edc37c4e
zazauwu <zazacrua@gmail.com>
2022-02-02 16:03:01 -0500

    Update MyCount.sh
    change for-loop

--- MyCount.sh
+++ MyCount.sh
@@ -2,12 +2,15 @@
 #The following command is to loop through a sequence of 20 numbers.
 #Within 1 to 20 print out the number and whether it is single- or double-digt
 
-for i in {1..9}
+for i in {1..20}
 do
+ #1 to 9 should be single-digit
+ if [ $i -lt 10 ] 
+ then
   echo "$i is a single-digit number"
-done
 
-for i in {10..20}
-do
+ else
+#10 to 20 should be double-digit
   echo "$i is a double-digit number"
+ fi
 done
